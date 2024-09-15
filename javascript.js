79355bb9b51df8599aa852b32adc065eb3db6bf2
RankineCyc <austinwilhite81601@gmail.com>
2024-09-14 21:28:13 -0700

    Add ability to update display

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
-console.log("Test");
+function eraseDisplayText() {
+    let displayTextField = document.querySelector("#numberDisplay");
+    displayTextField.textContent = 0;
+}
+
+function updateDisplayText() {
+    let displayTextField = document.querySelector("#numberDisplay");
+    let newDisplayText = this.textContent;
+
+    eraseDisplayText();
+    displayTextField.textContent = newDisplayText;
+}
+
+function performOperation() {
+    let desiredOperation = this.textContent;
+
+    if (desiredOperation == "AC"){
+        eraseDisplayText();
+    }
+}
+
+let allNumberButtons = document.querySelectorAll(".numberButton")
+allNumberButtons.forEach(element => {
+    element.addEventListener("click", updateDisplayText);
+});
+
+let allFunctionButtons = document.querySelectorAll(".functionButton");
+allFunctionButtons.forEach(element => {
+    element.addEventListener("click", performOperation);
+});
